1032ae0701219a14f3ffc376ce33875f5e06b97a
AdityaP183 <adityaprasad1828@gmail.com>
2024-07-28 11:30:41 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navbar } from './components/Navbar'
+import TodosList from './components/TodosList'
+
+export default function App() {
+  return (
+    <>
+     <Navbar/>
+     <TodosList/> 
+    </>
+  )
+}
